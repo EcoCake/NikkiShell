ffe630401d950014a7e27523e77aa7e89cc506e5
alicia <jade19955@gmail.com>
2025-08-27 16:35:18 +0000

    oops

--- src/tester.c
+++ src/tester.c
@@ -6,7 +6,7 @@
 /*   By: amezoe <amezoe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 14:17:16 by amezoe            #+#    #+#             */
-/*   Updated: 2025/08/27 16:09:03 by amezoe           ###   ########.fr       */
+/*   Updated: 2025/08/27 16:35:05 by amezoe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,18 +47,12 @@ int main(int ac, char **av, char **envp)
 
     while(1)
     {
-        // Get user input first.
         line = readline("nikkishell$ ");    
-
-        // If readline was interrupted by Ctrl+C, the flag will be set.
-        // We MUST update the status now, before processing the new line.
         if (g_last_signal == SIGINT)
         {
             last_exit_status = 130;
             g_last_signal = 0;
         }
-
-        // Now handle Ctrl+D (EOF)
         if (line == NULL)
         {
             printf("exit\n");
@@ -66,15 +60,11 @@ int main(int ac, char **av, char **envp)
             rl_clear_history();
             return (last_exit_status); 
         }
-
-        // Now handle empty/whitespace lines
         if (is_line_whitespace(line))
         {
             free(line);
             continue;
         }
-        
-        // From here on, last_exit_status is guaranteed to be up-to-date.
         if (*line)
         {
             add_history(line);
@@ -93,15 +83,12 @@ int main(int ac, char **av, char **envp)
                 last_exit_status = 2;
                 continue;
             }
-
-            // Expansion will now use the correct, updated status
             expand_cmd_args(commands, env_list, last_exit_status);
             expand_redirs(commands, env_list, last_exit_status);
-            
             last_exit_status = exec_main(commands, env_list, last_exit_status);
             free_cmd_list(commands);
         }
         free(line);
     }
-    return (last_exit_status); // This line is effectively unreachable
+    return (last_exit_status);
 }
