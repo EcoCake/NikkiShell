71889fa6ac9a5a96f2dde7b733d6b0d00ea78f1b
sionow <sionow@sionow.myguest.virtualbox.org>
2025-08-31 20:12:04 +0200

    bigger exit codes correct error msg

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: sionow <sionow@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 03:23:52 by sionow            #+#    #+#             */
-/*   Updated: 2025/08/30 17:51:39 by sionow           ###   ########.fr       */
+/*   Updated: 2025/08/31 20:10:53 by sionow           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	numchecker(char *str)
 	int	i;
 
 	i = 0;
-	if (ft_strcmp(str, "9223372036854775807") > 0)
+	if (ft_strlen(str) > 19 || ft_strcmp(str, "9223372036854775807") > 0)
 		return (1);
 	if ((str[i] == '-' || str[i] == '+') && ft_strlen(str) > 1)
 		i++;
